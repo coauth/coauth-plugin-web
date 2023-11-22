9653598c1ff38ab3ea554f4f3d7afe2217ee8f46
Godwin Pinto <godwin.pinto86@gmail.com>
2023-11-22 19:23:51 +0530

    fix: module reconfirm implementation

--- README.md
+++ README.md
@@ -1,52 +1,17 @@
-# coauth-plugin-web
+# Co-Auth Web plugin
 
-This template should help get you started developing with Vue 3 in Vite.
+This application is responsible for the co-auth UI (including registration and verifications). 
 
-## Recommended IDE Setup
+## What it does
+1. The application is invoked in an iframe from your frontend application
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+### Technologies
+1. Node 18+
+2. VueJS
+3. Typescript
+4. Tailwindcss
+5. Flowbite
+6. Vite
 
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
-
-If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
-
-1. Disable the built-in TypeScript Extension
-    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette
-    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
-2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Run Unit Tests with [Vitest](https://vitest.dev/)
-
-```sh
-npm run test:unit
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+### Installation / Contribution / and more
+Follow documentation available at [co-auth documentation website](https://documentation.coauth.dev).
--- src/components/reconfirm/ReconfirmVerifyModule.vue
+++ src/components/reconfirm/ReconfirmVerifyModule.vue
@@ -1,41 +1,115 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import SpinnerOverlay from '../utils/SpinnerOverlay.vue';
 import IconSpinner from '../icons/IconSpinner.vue';
 import SuccessMessage from '../utils/SuccessMessage.vue';
+import axios, { AxiosError } from 'axios';
+import ErrorOverlay from '../utils/ErrorOverlay.vue';
 
-defineProps<{
+const props = defineProps<{
   code: string
 }>()
+const isSubmitLoading = ref(false);
+const moduleLoading = ref(true);
+const showModule = ref(false);
+const showSuccess = ref(false);
+const isModuleError = ref(false);
+const inputValue = ref('')
+const submitErrorMessage = ref('');
+const actualValue = ref('');
 
-const actualValue = ref('blah blah');
+const loadPage = async (code: string) => {
+  const payload = {
+    appCode: code
+  }
+  try {
+    const response = await axios.post(`/api/coauth/module/reconfirm/verification/view`, payload);
+    if (response.status === 200) {
+      moduleLoading.value = false;
+      showModule.value = true;
+      actualValue.value = response.data.data;
+    } else {
+      isModuleError.value = true;
+    }
+  } catch (error) {
+    isModuleError.value = true;
+    moduleLoading.value = false;
+    console.log(error);
+  }
+}
 
-const isLoading = ref(false);
+const verifyRequest = async () => {
+  submitErrorMessage.value = '';
+  isSubmitLoading.value = true;
+  if (inputValue.value.length == 0) {
+    submitErrorMessage.value = 'Input value cannot be empty';
+    isSubmitLoading.value = false;
+    inputValue.value = '';
+    return;
+  }
+  const payload = {
+    appCode: props.code,
+    reconfirmInputText: inputValue.value
+  }
+  try {
+    inputValue.value = '';
+    const response = await axios.post(`/api/coauth/module/reconfirm/verification/authenticate`, payload);
+    if (response.status === 200) {
+      showModule.value = false;
+      showSuccess.value = true;
+      callParentTransactionComplete();
+    } else {
+      submitErrorMessage.value = response.data.error.message;
+    }
+    isSubmitLoading.value = false;
+    showModule.value = false;
+    showSuccess.value = true;
+  } catch (error: any) {
+    if (error.response && error.response.status === 400) {
+      submitErrorMessage.value = error.response.data.error.message;
+    } else {
+      submitErrorMessage.value = 'An unexpected error occurred.';
+    }
 
-const inputValue = ref('');
+    console.log(error);
+    isSubmitLoading.value = false;
+  }
+}
+
+const callParentTransactionComplete = () => {
+
+  if (window.parent) {
+    window.parent.postMessage('transactionComplete', '*');
+  }
+}
+
+onMounted(() => {
+  loadPage(props.code);
+})
 </script>
 
 <template>
-  <SpinnerOverlay />
-  <SuccessMessage :msg="'Verification successful. Please wait processing transaction...'" class="px-4" />
-  <div class="w-full ">
+  <SpinnerOverlay v-if="moduleLoading" />
+  <SuccessMessage :msg="'Verification successful. Please wait processing transaction...'" class="px-4"
+    v-if="showSuccess" />
+  <ErrorOverlay v-if="isModuleError" :msg="'Invalid request'" class="p-4" />
+  <div class="w-full " v-if="showModule">
     <div class="flex flex-col justify-center items-center mx-4">
       <div>
         <h5 class="w-full text-xl font-bold dark:text-white">Please authorize this Transaction</h5>
       </div>
-      <div class="mt-4 dark:border-cyan-300 w-full">
-        <label for="email" class="block mb-2 text-sm font-normal text-gray-900 dark:text-white">Re-type the word:
-          <span class="text-xl font-bold">{{ actualValue }}</span> in the input box below</label>
+      <div class="mt-4 dark:border-cyan-300 w-full mb-8">
+        <label for="email" class="block mb-2 text-sm font-normal text-gray-900 dark:text-white"
+          v-html="actualValue"></label>
         <input type="email" name="email" id="email" v-model="inputValue"
           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="" required="true">
-        <p class="mt-2 text-sm text-red-600 dark:text-red-500  mb-6"><span class="font-medium">Entered
-            value does not match</span></p>
+          <p class="mt-2 text-sm text-red-600 dark:text-red-500  mb-6" v-if="submitErrorMessage!=''"><span class="font-medium">{{ submitErrorMessage }}</span></p>
       </div>
-      <button type="button"
+      <button type="button" @click="verifyRequest"
         class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center justify-center">
-        <IconSpinner v-if="isLoading" />
-        Verify PIN
+        <IconSpinner v-if="isSubmitLoading" />
+        Submit
       </button>
     </div>
   </div>
